9a8cbe41bcf916a496e41da862c21a889112b1fd
Salman313ali <salman313ali@gmail.com>
2023-04-29 02:22:06 +0500

    creating intial module and connectors

---
+++ getxtodoapp/lib/app/data/services/storage/repository.dart
@@ -0,0 +1,11 @@
+import 'package:getxtodoapp/app/data/providers/task/privoder.dart';
+
+import '../../models/task.dart';
+
+class TaskRepository {
+  TaskProvider taskProvider;
+  TaskRepository({required this.taskProvider});
+
+  List<Task> readTasks() => taskProvider.readTasks();
+  void writeTasks(List<Task> tasks) => taskProvider.writeTasks(tasks);
+}
