b5ad2206f38790b6c12b81ecdf79f2007842b00e
Salman313ali <salman313ali@gmail.com>
2023-04-29 23:45:31 +0500

    added  add task function

--- getxtodoapp/lib/app/modules/home/controller.dart
+++ getxtodoapp/lib/app/modules/home/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:getxtodoapp/app/data/services/storage/repository.dart';
 
@@ -7,8 +8,14 @@ class HomeController extends GetxController {
   TaskRepository taskRepository;
   HomeController({required this.taskRepository});
 
+  final formKey = GlobalKey<FormState>();
+  final editCtrl = TextEditingController();
+  final chipIndex = 0.obs;
+  final deleting = false.obs;
   //what is obs??
   final tasks = <Task>[].obs;
+
+  final task = Rx<Task?>(null);
 
   //what is oninit??
   @override
@@ -20,6 +27,49 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {
+    editCtrl.dispose();
     super.onClose();
+  }
+
+  void changeChipIndex(int value) {
+    chipIndex.value = value;
+  }
+
+  void changeDeleting(bool value) {
+    deleting.value = value;
+  }
+
+  void changeTask(Task? select) {
+    task.value = select;
+  }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      return false;
+    }
+    tasks.add(task);
+    return true;
+  }
+
+  void deleteTask(Task task) {
+    tasks.remove(task);
+  }
+
+  bool updateTask(Task task, String title) {
+    var todos = task.todos ?? [];
+    if (containeTodo(todos, title)) {
+      return false;
+    }
+    var todo = {'title': title, 'done': false};
+    todos.add(todo);
+    var newTask = task.copyWith(todos: todos);
+    int oldIdx = tasks.indexOf(task);
+    tasks[oldIdx] = newTask;
+    tasks.refresh();
+    return true;
+  }
+
+  bool containeTodo(List todos, String title) {
+    return todos.any((element) => element['title'] == title);
   }
 }
--- getxtodoapp/lib/app/modules/home/view.dart
+++ getxtodoapp/lib/app/modules/home/view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:getxtodoapp/app/core/utils/extensions.dart';
+import 'package:getxtodoapp/app/core/values/colors.dart';
+import 'package:getxtodoapp/app/data/models/task.dart';
 import 'package:getxtodoapp/app/modules/home/controller.dart';
+import 'package:getxtodoapp/app/modules/home/widgets/add_card.dart';
+import 'package:getxtodoapp/app/modules/home/widgets/add_dialog.dart';
+import 'package:getxtodoapp/app/modules/home/widgets/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
@@ -9,21 +16,61 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(4.0.wp),
-            child: Text(
-              'My List',
-              style: TextStyle(
-                fontSize: 24.0.sp,
-                fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4.0.wp),
+              child: Text(
+                'My List',
+                style: TextStyle(
+                  fontSize: 24.0.sp,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
+            Obx(
+              () => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  ...controller.tasks
+                      .map((element) => LongPressDraggable(
+                          data: element,
+                          onDragStarted: () => controller.changeDeleting(true),
+                          onDraggableCanceled: (_, __) =>
+                              controller.changeDeleting(false),
+                          onDragEnd: (_) => controller.changeDeleting(false),
+                          feedback: Opacity(
+                            opacity: 0.8,
+                            child: TaskCard(task: element),
+                          ),
+                          child: TaskCard(task: element)))
+                      .toList(),
+                  AddCard()
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+      floatingActionButton: DragTarget<Task>(
+        builder: (_, __, ___) {
+          return Obx(
+            () => FloatingActionButton(
+              backgroundColor: controller.deleting.value ? Colors.red : blue,
+              onPressed: () =>
+                  Get.to(() => AddDialog(), transition: Transition.downToUp),
+              child: Icon(controller.deleting.value ? Icons.delete : Icons.add),
+            ),
+          );
+        },
+        onAccept: (Task task) {
+          controller.deleteTask(task);
+          EasyLoading.showSuccess('Delete Sucess');
+        },
+      ),
+    );
   }
 }
